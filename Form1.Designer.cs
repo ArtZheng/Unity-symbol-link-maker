fa55ad01e75237679f5924f3844185a7e181d4e9
dayweek11122 <dayweek11122@gmail.com>
2019-09-03 10:36:06 +0800

    Update and correction made to 'How to' instructions.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -62,7 +62,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(516, 52);
+            this.label1.Size = new System.Drawing.Size(472, 52);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
             // 
